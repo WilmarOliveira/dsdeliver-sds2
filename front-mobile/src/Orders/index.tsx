2da038ea3bb4fc454ac5f9eb377a597e60e3674f
Wilmar Oliveira <wilmar_oliveira13@hotmail.com>
2021-07-14 23:01:47 -0300

    Add Routes

---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from '../Header';
+
+export default function Orders() {
+
+  return (
+    <>
+      <Header />
+      <View>
+          <Text>Listagem de pedidos</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+});
